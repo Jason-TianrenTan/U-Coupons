182ba5f8306e56f00b6d7cf40ed9c5bb3a64cde0
Chidori <13522330013@163.com>
2017-07-14 15:51:15 +0800

    simulate max+

--- app/src/main/java/com/example/administrator/ccoupons/Main/MainActivity.java
+++ app/src/main/java/com/example/administrator/ccoupons/Main/MainActivity.java
@@ -1,26 +1,38 @@
 package com.example.administrator.ccoupons.Main;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.administrator.ccoupons.MainPageActivity;
 import com.example.administrator.ccoupons.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button login;
     Toolbar toolbar;
     EditText signup_phone, signup_pass;
+    private int mergeHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //    getSupportActionBar().hide();
 
-        toolbar = (Toolbar)findViewById(R.id.login_toolbar);
+        toolbar = (Toolbar) findViewById(R.id.login_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -39,11 +51,18 @@ public class MainActivity extends AppCompatActivity {
         signup_pass = (EditText) findViewById(R.id.signup_password);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+
         signup_phone.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
                     hideKeyboard(v);
+                    if (stateShrinked)
+                        startAnimation(ANIM_EXPAND);
+                }
+                else {
+                    if (!stateShrinked)
+                        startAnimation(ANIM_SHRINK);
                 }
             }
         });
@@ -52,9 +71,15 @@ public class MainActivity extends AppCompatActivity {
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
                     hideKeyboard(v);
+                    if (stateShrinked)
+                        startAnimation(ANIM_EXPAND);
+                } else {
+                    if (!stateShrinked)
+                        startAnimation(ANIM_SHRINK);
                 }
             }
         });
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,18 +87,75 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView text_forget = (TextView)findViewById(R.id.text_forget);
+        TextView text_forget = (TextView) findViewById(R.id.text_forget);
         text_forget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, ResetPasswordActivity.class));
             }
         });
+        LinearLayout rootLayout = (LinearLayout)findViewById(R.id.rootLayout);
+        rootLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                MainActivity.this.getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+                //获取屏幕的高度
+                int screenHeight =  MainActivity.this.getWindow().getDecorView().getRootView().getHeight();
+                //此处就是用来获取键盘的高度的， 在键盘没有弹出的时候 此高度为0 键盘弹出的时候为一个正数
+                int heightDifference = screenHeight - r.bottom;
+                System.out.println("Keyboard Size: " + heightDifference);
+            }
+        });
+        mergeHeight = dp2px(120);
     }
 
-    public void hideKeyboard(View view) {
+    private void hideKeyboard(View view) {
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+
+    private boolean stateShrinked = false;
+    private static final int ANIM_SHRINK = 0,
+                            ANIM_EXPAND = 1;
+    private void startAnimation(int anim_type) {
+
+        if (anim_type == ANIM_SHRINK) {
+
+            LinearLayout textRoot = (LinearLayout) findViewById(R.id.text_root_view);
+            ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(textRoot, "y", textRoot.getTop(), textRoot.getTop() - mergeHeight)
+                    .setDuration(500);
+            heightAnimator.start();
+
+            RelativeLayout imgLayout = (RelativeLayout) findViewById(R.id.imglayout);
+            Animation anim = new AnimationUtils().loadAnimation(this, R.anim.image_shrink);
+            anim.setFillAfter(true);
+            imgLayout.startAnimation(anim);
+            stateShrinked = true;
+        }
+        if (anim_type == ANIM_EXPAND) {
+
+            LinearLayout textRoot = (LinearLayout) findViewById(R.id.text_root_view);
+            ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(textRoot, "y", textRoot.getTop() - mergeHeight, textRoot.getTop())
+                    .setDuration(500);
+            heightAnimator.start();
+
+            RelativeLayout imgLayout = (RelativeLayout) findViewById(R.id.imglayout);
+            Animation anim = new AnimationUtils().loadAnimation(this, R.anim.image_expand);
+            anim.setFillAfter(true);
+            imgLayout.startAnimation(anim);
+            stateShrinked = false;
+        }
+
+
+    }
+
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getResources().getDisplayMetrics());
     }
 
 }
