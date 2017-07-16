178a5c93d4bb2f439a16e7740c3f9e8dba164067
Chidori <13522330013@163.com>
2017-07-16 10:23:39 +0800

    With location initial

--- app/src/main/java/com/example/administrator/ccoupons/Fragments/CategoryFragment.java
+++ app/src/main/java/com/example/administrator/ccoupons/Fragments/CategoryFragment.java
@@ -1,5 +1,7 @@
 package com.example.administrator.ccoupons.Fragments;
 
+import com.amap.api.location.AMapLocation;
+
 import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Color;
@@ -13,9 +15,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.bigkoo.convenientbanner.listener.OnItemClickListener;
@@ -28,23 +34,32 @@ import com.example.administrator.ccoupons.Search.SearchActivity;
 
 import java.util.ArrayList;
 
-public class CategoryFragment extends Fragment{
+public class CategoryFragment extends Fragment {
 
+
+    private AMapLocationClient mLocationClient;
+    private AMapLocationClientOption mLocationOption;
     private ArrayList<Category> categoryList;
     private CategoryAdapter adapter;
-    private TextView titleText;
+    private TextView location_text;
     private ArrayList<Integer> localImages;
     private ConvenientBanner convenientBanner;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(
                 R.layout.fragment_category, container, false);
 
-
-        convenientBanner = (ConvenientBanner)view.findViewById(R.id.category_banner);
+        convenientBanner = (ConvenientBanner) view.findViewById(R.id.category_banner);
         Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
-        EditText searchText = (EditText)view.findViewById(R.id.search_text);
+
+        location_text = (TextView)view.findViewById(R.id.location_textview);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)location_text.getLayoutParams();
+        params.addRule(RelativeLayout.ALIGN_BOTTOM, R.id.search_input_layout);
+        location_text.setLayoutParams(params);
+
+        EditText searchText = (EditText) view.findViewById(R.id.search_text);
         searchText.setFocusable(false);
         searchText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,11 +68,12 @@ public class CategoryFragment extends Fragment{
                 getActivity().startActivity(new Intent(getActivity(), SearchActivity.class));
             }
         });
-     //   SearchView searchView = (SearchView)view.findViewById(R.id.search_view);
+        //   SearchView searchView = (SearchView)view.findViewById(R.id.search_view);
         initCategory();
         initBanner();
-        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.category_recycler_view);
-        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(),2);//测试
+        initLocation();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.category_recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);//测试
         recyclerView.setLayoutManager(layoutManager);
         adapter = new CategoryAdapter(categoryList);
         recyclerView.setAdapter(adapter);
@@ -65,9 +81,9 @@ public class CategoryFragment extends Fragment{
     }
 
     //初始化展示板
-    private void initBanner(){
+    private void initBanner() {
         localImages = new ArrayList<>();
-        for (int i = 0; i< DataHolder.Banners.covers.length; i++)
+        for (int i = 0; i < DataHolder.Banners.covers.length; i++)
             localImages.add(DataHolder.Banners.covers[i]);
 
         //本地图片例子
@@ -80,7 +96,7 @@ public class CategoryFragment extends Fragment{
                     }
                 }, localImages)
                 .setPageIndicator(new int[]{R.mipmap.ic_page_indicator, R.mipmap.ic_page_indicator_focused});
-                //设置指示器的方向
+        //设置指示器的方向
 //                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
 //                .setOnPageChangeListener(this)//监听翻页事件
 
@@ -95,12 +111,35 @@ public class CategoryFragment extends Fragment{
     private void initCategory() {
         categoryList = new ArrayList<Category>();
         for (int i = 0; i < DataHolder.Categories.covers.length; i++) {
-            Category category = new Category(DataHolder.Categories.nameList[i],DataHolder.Categories.covers[i]);
+            Category category = new Category(DataHolder.Categories.nameList[i], DataHolder.Categories.covers[i]);
             categoryList.add(category);
         }
     }
 
+    public void initLocation() {
+        mLocationClient = new AMapLocationClient(getActivity().getApplicationContext());
+        mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setOnceLocation(true);
+        mLocationClient.setLocationOption(mLocationOption);
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setHttpTimeOut(20 * 1000);
+        mLocationOption.setNeedAddress(true);
 
+        AMapLocationListener aMapLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                if (aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                        String address = aMapLocation.getCity();
+                        location_text.setText(address);
+                    }
+                }
+            }
+        };
+        mLocationClient.setLocationListener(aMapLocationListener);
+
+        mLocationClient.startLocation();
+    }
 
 
 }
