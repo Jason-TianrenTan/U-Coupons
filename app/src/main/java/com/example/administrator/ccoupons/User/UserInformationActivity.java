d616944706f520b6ff46f645e532a97565f5817d
FishingKing <453814685@qq.com>
2017-07-24 18:12:38 +0800

    7/24 18

--- app/src/main/java/com/example/administrator/ccoupons/User/UserInformationActivity.java
+++ app/src/main/java/com/example/administrator/ccoupons/User/UserInformationActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.administrator.ccoupons.Data.DataHolder;
 import com.example.administrator.ccoupons.R;
+import com.example.administrator.ccoupons.Tools.DataBase.ImageLruCache;
 import com.example.administrator.ccoupons.Tools.DataBase.LoginInformationManager;
 import com.example.administrator.ccoupons.Tools.SlideBackActivity;
 import com.example.administrator.ccoupons.Tools.TakePhotoUtil;
